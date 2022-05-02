93958e25cd79cf17aabff36d426cdd77d7bfc2f7
Davies <daviesaraujo@hotmail.com>
2022-05-02 06:42:42 -0300

    estilizando melhor alguns components

--- react-study/src/components/tailwindcss/index.tsx
+++ react-study/src/components/tailwindcss/index.tsx
@@ -1,5 +1,5 @@
 export const Tailwindtest = () => {
     return (
-        <div className="bg-black text-white text-center">...</div>
+        <div className="bg-black text-white text-center p-5">...</div>
     )
 }
